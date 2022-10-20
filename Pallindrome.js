814687b5d91ddd44a3d76e6bf4e4962b4d9fd8a9
gzbsingh <gentalman1945@gmail.com>
2022-10-20 15:20:35 +0530

    first commit

---
+++ Pallindrome.js
@@ -0,0 +1,9 @@
+let str="abcd";
+let bagr=""
+   for(let j=str.length-1;j>=0;j++){
+      bagr=bagr+str[j];
+  }
+if(str==bagr)
+ console.log("Yes");
+else 
+console.log("No);
---
+++ Prime.js
@@ -0,0 +1,11 @@
+ let is=true;
+    for(let i=2;i<num;i++)
+    {
+       
+        if(num%i!=0)
+         is=false;
+    }
+    if(is)
+    console.log("Yes");
+    else
+    console.log("No");
